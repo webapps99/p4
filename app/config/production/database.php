8b62ece4873aefb898b4d096811d914c4dd5f69c
Chris Nystrom <chrisnys_88@hotmail.com>
2014-12-16 18:13:34 -0500

    Config Live Database

---
+++ app/config/production/database.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cnystrom
+ * Date: 12/16/2014
+ * Time: 6:09 PM
+ */
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'pets',
+            'username'  => 'root',
+            'password'  => '8m1DPNAt7W',
+        ),
+
+    ),
+
+);
